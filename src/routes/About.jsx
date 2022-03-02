a17a05aec5412681595a41e8cd6e7fe8d806bb4e
RiceDaddy9 <jukke.dev@gmail.com>
2022-03-01 23:29:06 -0800

    Draft concepts for About && Portfolio pages

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -10,6 +10,32 @@ export default function About() {
       style={{backgroundImage: `url(${Banner})`}}>
         
       </div>
+      <div>
+        <p>About me</p>
+        <p>
+          I am a self taught Programmer. I currently work about 50 hours a week as an assistant manager. I've bought a book on
+          HTML && CSS and two books on Javascript. I've done online courses on web development. I am an aspiring Frontend developer.
+        
+        </p>
+      </div>
+        <div style={{display: 'flex', justifyContent: "center", alignItems: "center", width: '100%', margin: '30px 0'}}>
+          <div style={{height: "50vh", width: '80%', backgroundColor: "green"}}>
+          </div>
+        </div>
+      <div>
+        Goal: I want to be a full stack developer and in the future, I want to learn unity.
+      </div>
+      <div>
+        <div>
+          HTML
+          CSS,
+          ES6,
+          React
+        </div>
+        <div>
+          Experienced in Node, Express, MongoDB, Firebase
+        </div>
+      </div>
     </div>
   )
 }
