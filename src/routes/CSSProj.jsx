79af8e6279a937925d8febbaf75caa2a9cd50e56
RiceDaddy9 <jukke.dev@gmail.com>
2022-03-05 23:24:13 -0800

    Add button animation to /css-animation page

--- src/routes/CSSProj.jsx
+++ src/routes/CSSProj.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
-import style from '../PageStyles/Buttons.module.css'
+import style from '../PageStyles/AnimationStyleSheet/btn.module.css'
 
 export default function CSSProj() {
   return (
     <div className='CSScntnr'>
       <div><h1>CSS Animation Projects</h1></div>
-      <div className='buttons'>
-        buttons
+      <div className={style.btnAnimate}>
+        <div>
+          <a className={style.btn00}>Hover Me</a>
+        </div>
+        <div>
+          <a className={style.btn01}>Hover Me</a>
+        </div>
+        <div>
+          <a className={style.btn02}><span></span>Hover Me</a>
+        </div>
+        <div>
+          <a className={style.btn03}>Hover Me</a>
+        </div>
+        <div>
+          <a className={style.btn04}>Hover Me</a>
+        </div>
+        <div>
+          <a className={style.btn05}>Hover Me</a>
+        </div>
+        <div>
+          <a className={style.btn06}>Hover Me</a>
+        </div>
+        <div>
+          <a className={style.btn07}>Hover Me</a>
+        </div>
       </div>
       <div className='cards'>
         cards
