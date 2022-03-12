055e3ae77824e7263534fd12547661f85fdc2043
RiceDaddy9 <jukke.dev@gmail.com>
2022-03-12 08:47:44 -0800

    fix homepage responsive design

--- src/index.js
+++ src/index.js
@@ -8,13 +8,11 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
 //BrowserRouter wraps <App /> to route between pages
-  <div style={{backgroundImage: `${background}`}}>
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </React.StrictMode>
-  </div>,
+    </React.StrictMode>,
     document.getElementById('root')
 );
 
