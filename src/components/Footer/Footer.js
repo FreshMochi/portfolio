1994b1ec1dc264e34d07bcd81ebbc905782c300f
RiceDaddy9 <jukke.dev@gmail.com>
2022-03-16 12:07:15 -0700

    fixed email info and gmail link to jukke.dev@gmail.com

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -21,14 +21,14 @@ export default function Footer() {
             href="https://github.com/FreshMochi" target="_blank">
             </a>
             
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=nxiong984@gmail.com&su=SUBJECT&body=BODY" target="_blank">
+            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=jukke.dev@gmail.com&su=SUBJECT&body=BODY" target="_blank">
             <div className={style.smIcon} style={{backgroundImage: `url(${gmailPng})`}}> </div>
             </a>
           </div>
           <div className={style.ftInfo}>
             <div className={style.contactInfo}>
               <p>Location: San Jose</p>
-              <p>Email: nxiong984@gmail.com</p>
+              <p>Email: jukke.dev@gmail.com</p>
               <div className={style.footerLogo}>
                 <img style={{height: "inherit"}} src={logo} />
               </div>
